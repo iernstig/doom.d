0bb2263fc5dc833d0a171fd33e43af8dbdb5cf9d
Isak Ernstig <ernstig1@gmail.com>
2019-08-21 12:42:31 -0400

    lsp for python works

--- +py.el
+++ +py.el
@@ -8,28 +8,3 @@
   (python-shell-send-buffer)
   )
 
-;; LSP-configuration
-;; (def-package! lsp-mode
-;;               :ensure t
-;;               :config
-
-;;               (require 'lsp-menu)
-;;               (lsp-define-stdio-client lsp-python "python"
-;;                                        #'projectile-project-root
-;;                                        '("pyls"))
-
-;;               (add-hook 'python-mode-hook
-;;                         lambda ()
-;;                         (lsp-python-enable)))
-
-;; ;; LSP-extras
-;; (def-package! lsp-ui
-;;               :ensure t
-;;               :config
-;;               (setq lsp-ui-sideline-ignore-duplicate t)
-;;               (add-hook 'lsp-mode-hook 'lsp-ui-mode))
-
-;; (def-package! company-lsp
-;;               :config
-;;               (push 'company-lsp company-backends))
-;; TODO: fix the lsp for python
